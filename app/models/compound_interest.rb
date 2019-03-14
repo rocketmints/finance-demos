30a81bdb2103fa6aec142b892024d5f5c031f0ea
rocketmints <48554507+rocketmints@users.noreply.github.com>
2019-03-14 20:11:33 +0800

    Update compound_interest.rb

--- app/models/compound_interest.rb
+++ app/models/compound_interest.rb
@@ -2,7 +2,8 @@ class CompoundInterest
 
   include ActiveModel::Model
 
-  attr_accessor :current_principal, :years_to_grow, :interest_rate
+  attr_accessor :current_principal, :years_to_grow, :interest_rate,
+  :annual_addition
 
   attr_reader :result
 
@@ -18,7 +19,7 @@ class CompoundInterest
   private
 
   def calculate_result
-    future_value_of_principal
+    future_value_of_principal + future_value_of_additions
   end
 
   def future_value_of_principal
